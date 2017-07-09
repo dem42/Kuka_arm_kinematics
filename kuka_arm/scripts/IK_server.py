6b09bd29cf6ef855546451e63e7434003ae83c9c
martin papanek <martin.papanek@gmail.com>
2017-07-09 11:59:50 -0700

    inverse kinematics adjusting parameters

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -15,7 +15,7 @@ import tf
 from kuka_arm.srv import *
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
-from mpmath import *
+#from mpmath import *
 from sympy import *
 import timeit
 
@@ -57,48 +57,67 @@ def handle_calculate_IK(req):
             
             # Create individual transformation matrices
             print("computing all the nice transforms")
-            T0_1 = dh_transform(q1, alpha0, a0, d1)
-            T1_2 = dh_transform(q2, alpha1, a1, d2)
-            T2_3 = dh_transform(q3, alpha2, a2, d3)
-            T3_4 = dh_transform(q4, alpha3, a3, d4)
-            T4_5 = dh_transform(q5, alpha4, a4, d5)
-            T5_6 = dh_transform(q6, alpha5, a5, d6)
-            T6_G = dh_transform(q7, alpha6, a6, d7)
+
+            R0_1 = dh_rotation(q1, alpha0, a0, d1)
+            R1_2 = dh_rotation(q2, alpha1, a1, d2)
+            R2_3 = dh_rotation(q3, alpha2, a2, d3)
+            R3_4 = dh_rotation(q4, alpha3, a3, d4)
+            R4_5 = dh_rotation(q5, alpha4, a4, d5)
+            R5_6 = dh_rotation(q6, alpha5, a5, d6)
+            R6_G = dh_rotation(q7, alpha6, a6, d7)
+            
+            # T0_1 = dh_transform(q1, alpha0, a0, d1)
+            # T1_2 = dh_transform(q2, alpha1, a1, d2)
+            # T2_3 = dh_transform(q3, alpha2, a2, d3)
+            # T3_4 = dh_transform(q4, alpha3, a3, d4)
+            # T4_5 = dh_transform(q5, alpha4, a4, d5)
+            # T5_6 = dh_transform(q6, alpha5, a5, d6)
+            # T6_G = dh_transform(q7, alpha6, a6, d7)
 
             print("subs")
-            T0_1 = T0_1.subs(s)
-            T1_2 = T1_2.subs(s)
-            T2_3 = T2_3.subs(s)
-            T3_4 = T3_4.subs(s)
-            T4_5 = T4_5.subs(s)
-            T5_6 = T5_6.subs(s)
-            T6_G = T6_G.subs(s)
+            R0_1 = R0_1.subs(s)
+            R1_2 = R1_2.subs(s)
+            R2_3 = R2_3.subs(s)
+            R3_4 = R3_4.subs(s)
+            R4_5 = R4_5.subs(s)
+            R5_6 = R5_6.subs(s)
+            R6_G = R6_G.subs(s)
+            
+            # T0_1 = T0_1.subs(s)
+            # T1_2 = T1_2.subs(s)
+            # T2_3 = T2_3.subs(s)
+            #T3_4 = T3_4.subs(s)
+            #T4_5 = T4_5.subs(s)
+            #T5_6 = T5_6.subs(s)
+            #T6_G = T6_G.subs(s)
 
             print("simplifying")
             start_time = timeit.default_timer()
-            T0_2 = T0_1 * T1_2
-            T0_3 = T0_2 * T2_3
-            T0_4 = T0_3 * T3_4
-            T0_5 = T0_4 * T4_5
-            T0_6 = T0_5 * T5_6
-            T0_G = T0_6 * T6_G
+            R0_2 = R0_1 * R1_2
+            R0_3 = simplify(R0_2 * R2_3)
+
+            R3_5 = R3_4 * R4_5
+            R3_6 = R3_5 * R5_6
+            R3_G = R3_6 * R6_G
+            
+            # T0_2 = T0_1 * T1_2
+            # T0_3 = simplify(T0_2 * T2_3)
+            #T0_4 = T0_3 * T3_4
+            #T0_5 = T0_4 * T4_5
+            #T0_6 = T0_5 * T5_6
+            #T0_G = T0_6 * T6_G
             end_time = timeit.default_timer() - start_time
             print("simplifying took {0}s".format(end_time))
 
             print("correction matrix simplify")
-            R_z = Matrix([[cos(pi), -sin(pi), 0, 0],
-                          [sin(pi),  cos(pi), 0, 0],
-                          [      0,        0, 1, 0],
-                          [      0,        0, 0, 1]])
-            R_y = Matrix([[ cos(-pi/2),        0, sin(-pi/2), 0],
-                          [          0,        1,          0, 0],
-                          [-sin(-pi/2),        0, cos(-pi/2), 0],
-                          [          0,        0,          0, 1]])
+            R_z = rot_z(pi)
+            R_y = rot_y(-pi/2)
             R_corr = simplify(R_z * R_y)
 
             print("total matrix simplify")
             start_time = timeit.default_timer()
-            T0_total = simplify(T0_G * R_corr)
+            R3_total = simplify(R3_G * R_corr)
+            #T0_total = simplify(T0_G * R_corr)
             end_time = timeit.default_timer() - start_time
             print("simplifying total matrix took {0}s".format(end_time))
             
@@ -123,32 +142,42 @@ def handle_calculate_IK(req):
             Rrpy = R_roll * R_pitch * R_yaw
             n = Rrpy.col(2)
 
-            d67 = s[d6] + s[d7]
+            d67 = d6 + d7
             wx = px - (d67) * n[0]
             wy = py - (d67) * n[1]
             wz = pz - (d67) * n[2]
             print("Got wrist center ({0},{1},{2})".format(wx, wy, wz))
 
             theta1 = atan2(wy, wx)
-            r = sqrt(wy**2 + wx**2)
-            dia = sqrt(r**2 + (wz - s[d1])**2)
-            ang1 = atan2(wz - s[d1], r)
-            ang2 = arg_law_of_cosine(s[a2], s[a1], dia)
-            ang3 = arg_law_of_cosine(dia, s[a1], s[a2])
+            r = sqrt(wy**2 + wx**2) - a1
+            vert_d = (wz - a3 - d1)
+            dia = sqrt(r**2 + vert_d**2)
+            ang1 = atan2(vert_d, r)
+            ang2 = arg_law_of_cosine(d4, a2, dia)
+            ang3 = arg_law_of_cosine(dia, a2, d4)
             theta2 = ang2 + ang1
             theta3 = ang3 - pi
-            print("Got first three angles ({0},{1},{2})".format(theta1, theta2, theta3))
+            print("what? ({0},{1},{2},{3},{4}".format(r.evalf(subs=s), dia.evalf(subs=s), ang1.evalf(subs=s), ang2.evalf(subs=s), ang3.evalf(subs=s)))
+            print("Got first three angles ({0},{1},{2})".format(theta1.evalf(subs=s), theta2.evalf(subs=s), theta3.evalf(subs=s)))
 
-            T0_3s = T0_3.subs({q1: theta1, q2: theta2, q3: theta3})
-            T0_3s.col_del(-1)
-            T0_3s.row_del(-1)
-            R3_6 = T0_3s.T * Rrpy
-            print("Got the final 3_6 matrix {0}".format(R3_6))
+            R0_3s = R0_3.subs({q1: theta1, q2: theta2, q3: theta3})
+            R3_6 = R0_3s.T * Rrpy
+
+            # T0_3s = T0_3.subs({q1: theta1, q2: theta2, q3: theta3})
+            # T0_3s.col_del(-1)
+            # T0_3s.row_del(-1)
+            # R3_6 = T0_3s.T * Rrpy
             
-            
+            print("Got the final 3_6 matrix {0}\nCompare vs {1}".format(R3_6.evalf(subs=s),R3_G.evalf(subs=s)))
+            theta5 = acos(R3_6[1,2])
+            sin5 = sin(theta5)
+            theta4 = asin(R3_6[2,2] / sin5)
+            theta6 = acos(R3_6[1,0] / sin5)
+
+            print("about to return the values ({0},{1},{2})".format(theta1.evalf(subs=s), theta2.evalf(subs=s), theta3.evalf(subs=s)))
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+	    joint_trajectory_point.positions = [theta1.evalf(subs=s), theta2.evalf(subs=s), theta3.evalf(subs=s), theta4.evalf(subs=s), theta5.evalf(subs=s), theta6.evalf(subs=s)]
 	    joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
@@ -164,6 +193,11 @@ def dh_transform(q, alpha, a, d):
                    [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
                    [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
                    [                0,                 0,           0,             1]])
+
+def dh_rotation(q, alpha, a, d):
+    return Matrix([[           cos(q),           -sin(q),           0],
+                   [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha)],
+                   [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha)]])
 
 def rot_x(q1):
     return Matrix([[ 1,              0,        0],
